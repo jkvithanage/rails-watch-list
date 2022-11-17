d85b8ac067d9c637e396070ee694be75a4d5e139
Janaka Vithanage <jkvithana@gmail.com>
2022-11-17 21:11:01 +1100

    Added more styles

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,10 +1,12 @@
 class ListsController < ApplicationController
   def index
     @lists = List.all
+    @list = List.new
   end
 
   def show
     @list = List.find(params[:id])
+    @bookmark = Bookmark.new
   end
 
   def new
--- config/routes.rb
+++ config/routes.rb
@@ -12,8 +12,10 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   resources :movies, only: %i[show]
-  resources :lists, only: %i[index show new create] do
-    resources :bookmarks, only: %i[new create]
+  resources :lists, only: %i[index show create] do
+    resources :bookmarks, only: %i[create]
   end
   resources :bookmarks, only: [:destroy]
+
+  root "lists#index"
 end
